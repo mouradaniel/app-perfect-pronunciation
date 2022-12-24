720674833dec84b733cb8b035945e2ca7f9abfd1
Daniel Moura <daniel-moura@outlook.com>
2022-12-24 15:09:15 -0300

    feat: init welcome screen

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,12 @@
+import {
+  Container,
+  Title
+} from './styles';
+
+export function Welcome() {
+  return (
+    <Container>
+      <Title>Seja bem-vindo(a) ao Perfect Pronunciation!</Title>
+    </Container>
+  )
+}
