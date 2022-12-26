77e38d818d215f21e4f992f2273db95e4665fdee
Daniel Moura <daniel-moura@outlook.com>
2022-12-25 21:14:19 -0300

    feat: implement button navigate to home

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,16 +1,32 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { Header } from '../../components/Header';
 
 import {
   Container,
-  Title
+  Title,
+  Button,
+  ButtonText
 } from './styles';
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  function handleHome() {
+    navigation.navigate('home');
+  }
+
   return (
     <Container>
       <Header/>
 
       <Title>Seja bem-vindo(a) ao Perfect Pronunciation!</Title>
+
+      <Button
+        onPress={handleHome}
+      >
+        <ButtonText>Praticar</ButtonText>
+      </Button>
     </Container>
   )
 }
