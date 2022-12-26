7a8a33f526b30dd3302abd49cf3b06a04d71dd7f
Daniel Moura <daniel-moura@outlook.com>
2022-12-25 21:53:21 -0300

    feat: implement safe area view

--- src/screens/Welcome/styles.ts
+++ src/screens/Welcome/styles.ts
@@ -1,10 +1,11 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.WHITE};
+  padding: 30px;
   align-items: center;
-  justify-content: center;
 `;
 
 export const Title = styled.Text`
