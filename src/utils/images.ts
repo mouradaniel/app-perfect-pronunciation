d74cb65d0084caed5a4bb1564f3d67c5541f7748
Daniel Moura <daniel-moura@outlook.com>
2022-12-28 18:51:20 -0300

    feat: create object to reference link images

---
+++ src/utils/images.ts
@@ -0,0 +1,9 @@
+const images = {
+  play: require('../assets/botao-play-active.png'),
+  play_deactive: require('../assets/botao-play-deactive.png'),
+  logo: require('../assets/logo.png'),
+  microphone: require('../assets/microphone-deactive.png'),
+  microphone_deactive: require('../assets/microphone.png')
+};
+
+export default images;
