a518f18d56fc5e5b6bdf07f95393c1ba2d07697d
Daniel Moura <daniel-moura@outlook.com>
2022-12-28 17:59:27 -0300

    feat: create input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,17 @@
+import { TextInput } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled(TextInput)`
+  flex: 1;
+
+  min-height: 50px;
+  max-height: 50px;
+
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.GRAY_200};
+  padding-bottom: 8px;
+  padding-left: 8px;
+
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
+`;
