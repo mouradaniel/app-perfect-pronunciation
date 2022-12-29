41ad499d783b5eaeaeef1a3d4256612941476f62
Daniel Moura <daniel-moura@outlook.com>
2022-12-29 18:14:22 -0300

    feat: create a route for select language screen

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Welcome } from '@screens/Welcome';
 import { Home } from '@screens/Home';
+import { SelectLanguage } from '@screens/SelectLanguage';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -18,6 +19,11 @@ export function AppRoutes() {
       <Screen 
         name="home"
         component={Home} 
+      />
+
+      <Screen
+        name="select-language"
+        component={SelectLanguage}
       />
     </Navigator>
   );
