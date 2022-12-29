812385abb365d725b3af64abde79a97bab67b683
Daniel Moura <daniel-moura@outlook.com>
2022-12-29 18:13:23 -0300

    feat: init a select language screen

---
+++ src/screens/SelectLanguage/index.tsx
@@ -0,0 +1,14 @@
+import {
+  Container,
+  Title,
+  Subtitle
+} from './styles';
+
+export function SelectLanguage() {
+  return (
+    <Container>
+      <Title>O que deseja praticar?</Title>
+      <Subtitle>Selecione um dos idiomas abaixo para praticarmos.</Subtitle>
+    </Container>
+  )
+}
